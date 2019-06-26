1a588aabf1d7df4a969a7f87400ca008f8eecc41
Hédi <hedi.sassi@epfl.ch>
2019-06-26 13:55:43 +0200

    main server functions written. Not yet tested. Crypto isn't implemented yet

---
+++ server/crypto.py
@@ -0,0 +1,16 @@
+
+#crypto functions
+
+
+def encrypt(msg):
+
+    #TODO encrypt message with symmetric (pre shared) key on local computer
+
+    return msg
+
+
+def decrypt(msg):
+
+    # TODO decrypt message with symmetric (pre shared) key on local computer
+
+    return msg
---
+++ server/server_main.py
@@ -0,0 +1,66 @@
+
+
+import socket               # Import socket module
+import crypto
+import threading
+
+
+MSG_BUFFER_SIZE = 30
+NEW_CONN_TIMEOUT = 0.01
+MAX_PENDING_CONN = 5
+SERVER_PORT = 12345
+
+
+def main():
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
+    host = socket.gethostname()                                   # Get local machine name
+
+    s.bind((host, SERVER_PORT))        # Bind to the port
+
+    clients = []                # will contain clients data
+
+    msg_buffer = []             # message buffer
+
+    s.listen(MAX_PENDING_CONN)                 # Now wait for client connection with limit on pending connections
+
+    while True:
+
+        thread = threading.Thread(acquire_new_connections(s, clients))
+        thread.start()
+
+        thread.join(NEW_CONN_TIMEOUT)
+
+        print 'Got connection from', addr
+        msg = 'Thank you for connecting'
+        c.send(crypto.encrypt(msg))
+
+        for c in clients:
+
+            msg_from_client = crypto.decrypt(c.recv())         # receive message from client  recv(1024) ??
+
+            if not msg_from_client:
+                break
+
+            if msg_from_client == "!close":         # client requested connection to be closed
+                clients.remove(c)                   # remove client
+                c.close()                           # Close the connection
+
+            elif msg_from_client == "!refresh":     # Send
+                c.sendAll(msg_buffer)
+
+            else:
+                msg_buffer.append(msg_from_client)            # add to messages
+
+                if len(msg_buffer) > MSG_BUFFER_SIZE:         # if buffer is full, remove oldest message
+                    msg_buffer.remove(0)
+
+
+if __name__ == '__main__':
+    main()
+
+
+# listen for new connection and add it to the connections array
+def acquire_new_connections(s, clients):
+    c, addr = s.accept()  # Establish connection with client.
+    clients.append(c)
