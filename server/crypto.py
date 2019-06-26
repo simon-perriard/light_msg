e2f6740527dc06137e67b688c84c4ea8ad33563b
Hédi <hedi.sassi@epfl.ch>
2019-06-26 17:56:22 +0200

    support concurrent users

--- server/crypto.py
+++ server/crypto.py
@@ -4,17 +4,19 @@ from Crypto.Cipher import AES
 from Crypto import Random
 
 
+PATH_TO_KEY= "../../key"
+
 BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s) - 1:])]
 
-#crypto functions inspired by :
+# crypto functions inspired by :
 # https://www.quickprogrammingtips.com/python/aes-256-encryption-and-decryption-in-python.html
 
 
 def encrypt(msg):
 
-    private_key = b'AEQ124hetrknshet'
+    private_key = read_key(PATH_TO_KEY)
 
     raw = pad(msg)
     iv = Random.new().read(AES.block_size)
@@ -25,9 +27,18 @@ def encrypt(msg):
 
 def decrypt(msg):
 
-    private_key = b'AEQ124hetrknshet'
+    private_key = read_key(PATH_TO_KEY)
 
     enc = base64.b64decode(msg)
     iv = enc[:16]
     cipher = AES.new(private_key, AES.MODE_CBC, iv)
     return unpad(cipher.decrypt(enc[16:]))
+
+
+def read_key(path):
+    f = open(path, "r")
+
+    if f.mode == 'r':
+        return f.read(16).encode()
+
+    exit(-1)
--- server/server_main.py
+++ server/server_main.py
@@ -2,7 +2,6 @@
 
 import socket               # Import socket module
 import crypto
-import threading
 
 
 MSG_BUFFER_SIZE = 30
@@ -11,20 +10,39 @@ MAX_PENDING_CONN = 5
 SERVER_PORT = 12345
 
 
-# listen for new connection and add it to the connections array
-def acquire_new_connections(s, clients):
-    c, addr = s.accept()  # Establish connection with client.
-    clients.append(c)
-    print("new connection !")
+def flush_buffer(msg_buffer, c):
 
+    for i in range(0, len(msg_buffer)):
+        c.send(crypto.encrypt(msg_buffer[i]))
+
+def create_socket():
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', SERVER_PORT))  # Bind to the port
+
+    s.listen(MAX_PENDING_CONN)  # Now wait for client connection with limit on pending connections
+
+    return s
+
+
+# listen for new connection and add it to the connections array
+def acquire_new_connections(s):
+
+    c, _ = s.accept()  # Establish connection with client.
+
+    if not c:
+        return
+
+    print("new connection !")
+    return c
 
 
 def main():
 
-    print("Starting server ...")
+    print("Starting server...")
 
-
-    print("Testing encryption....")
+    print("Testing encryption...")
 
     test = 'secret message'
     encrypted = crypto.encrypt(test)
@@ -36,47 +54,34 @@ def main():
         print("Connection is not secure. Aborting.")
         exit(-1)
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)         # Create a socket object
-
-    s.bind(('', SERVER_PORT))        # Bind to the port
-
-    clients = []                # will contain clients data
-
     msg_buffer = []             # message buffer
 
-    s.listen(MAX_PENDING_CONN)                 # Now wait for client connection with limit on pending connections
+    s = create_socket()
 
     while True:
 
-        thread = threading.Thread(target=acquire_new_connections, args=(s, clients,))  # check for new connections
+        c = acquire_new_connections(s)  # check for new connections
 
-        thread.start()
-        thread.join(NEW_CONN_TIMEOUT)
+        buffer = c.recv(1024)
 
-        if thread.is_alive():
-            thread.join()
+        if not buffer:
+            break
 
-        for c in clients:
+        msg_from_client = crypto.decrypt(buffer)        # receive message from client
+        print(msg_from_client)
 
-            msg_from_client = crypto.decrypt(c.recv(1024))        # receive message from client  recv(1024) ??
+        if not msg_from_client:
+            break
 
-            if not msg_from_client:
-                break
+        if msg_from_client == b'!refresh':     # Send latest messages
+            flush_buffer(msg_buffer, c)
+            print("request refresh")
 
-            if msg_from_client == b'!close':         # client requested connection to be closed
-                clients.remove(c)                   # remove client
-                c.close()                           # Close the connection
-                print("close connection")
+        else:
+            msg_buffer.append(msg_from_client)            # add to messages
 
-            elif msg_from_client == b'!refresh':     # Send latest messages
-                c.sendAll(msg_buffer)
-                print("request refresh")
-
-            else:
-                msg_buffer.append(msg_from_client)            # add to messages
-
-                if len(msg_buffer) > MSG_BUFFER_SIZE:         # if buffer is full, remove oldest message
-                    msg_buffer.remove(0)
+            if len(msg_buffer) > MSG_BUFFER_SIZE:         # if buffer is full, remove oldest message
+                msg_buffer.remove(0)
 
 
 if __name__ == '__main__':
